96518badd67cc7b616247987f6c84480e2b90fc3
Maimun54 <maimunislam@gmail.com>
2024-01-09 00:31:07 +0600

    recap concat menthod to join tow array

--- SpreadOparator.js
+++ SpreadOparator.js
@@ -4,4 +4,16 @@ function calculate(a,b,c,d){
     console.log(`Sum: `+(a+b+c+d));
 }
 // calculate(5,10,15,20)
-calculate.apply(null, numbers)
+// calculate.apply(null, numbers)
+calculate(...numbers)
+
+
+
+
+
+
+
+let arrOne =[0,1,2,3,4,5]
+let arrTwo =[6,7,8,9,10]
+arrOne =arrOne.concat(arrTwo)
+console.log(arrOne)
