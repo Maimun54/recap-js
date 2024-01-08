9fa36634808de20340735bb3488a340a62c2ed4b
Maimun54 <maimunislam@gmail.com>
2024-01-08 22:56:30 +0600

    first commit

---
+++ Array/maimun.js
@@ -0,0 +1,14 @@
+var a =10
+console.log(a)
+
+var arrayName = new Array('Karim','rahim','jabbar')
+arrayName[2]="Maimun"
+console.log(arrayName)
+
+var array = ['Karim','rahim','jabbar']
+console.log(array.length)
+console.log(array[1])
+
+for(var i =0 ;i<array.length; i=i+1){
+    console.log('Name:' + array[i])
+}
