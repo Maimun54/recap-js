bf5fe51a8e3a9642de27d6422026053d68386a4f
Maimun54 <maimunislam@gmail.com>
2024-01-18 01:17:32 +0600

    Done

--- SpreadOparator.js
+++ SpreadOparator.js
@@ -3,8 +3,8 @@ let numbers = [1,2,3,4,5]
 function calculate(a,b,c,d){
     console.log(`Sum: `+(a+b+c+d));
 }
-// calculate(5,10,15,20)
-// calculate.apply(null, numbers)
+calculate(5,10,15,20)
+calculate.apply(null, numbers)
 calculate(...numbers)
 
 
